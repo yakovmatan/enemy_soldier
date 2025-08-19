5211fc92d3f14af0a56631b46c3828300d333b75
yakov matan <yakovmatan@gmail.com>
2025-08-19 20:19:32 +0300

    create function insert_soldier

--- src/dal.py
+++ src/dal.py
@@ -1,11 +1,21 @@
 from pymongo.errors import PyMongoError
 from src.db_connection import DbConnection
+from src.soldier import Soldier
 
 
 class Dal:
 
     def __init__(self):
         self.connection = DbConnection()
+
+    @staticmethod
+    def check_soldier(soldier_dict: dict):
+        try:
+            soldier_obj = Soldier(soldier_dict['soldier_id'],soldier_dict['first_name'],soldier_dict['last_name'],soldier_dict['phone_number'], soldier_dict['rank'])
+            return soldier_obj.__dict__
+        except Exception as e:
+            print(f"Invalid soldier data: {e}")
+            return None
 
     def find_all(self):
         try:
@@ -14,3 +24,16 @@ class Dal:
         except PyMongoError as e:
             print(f"Error reading data: {e}")
             return []
+
+    def insert_soldier(self, soldier: dict):
+        try:
+            valid_soldier = Dal.check_soldier(soldier)
+            if not valid_soldier:
+                print("Soldier validation failed. Not inserting.")
+                return None
+
+            collection = self.connection.db[self.connection.collection_name]
+            return collection.insert_one(valid_soldier).inserted_id
+        except PyMongoError as e:
+            print(f"Error inserting data: {e}")
+            return None
