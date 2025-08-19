34337b1a2fe98f87a0b2cf048494cb9077e8896a
yakov matan <yakovmatan@gmail.com>
2025-08-19 17:50:57 +0300

    create function to get all documents

--- src/dal.py
+++ src/dal.py
@@ -7,3 +7,10 @@ class Dal:
     def __init__(self):
         self.connection = DbConnection()
 
+    def find_all(self):
+        try:
+            collection = self.connection.db[self.connection.collection_name]
+            return list(collection.find({}, {"_id": 0}))
+        except PyMongoError as e:
+            print(f"Error reading data: {e}")
+            return []
